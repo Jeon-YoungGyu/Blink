09d9eeed722f13489877cc3d1c446f8cea24c1bd
Jeon-YoungGyu <55074084+Jeon-YoungGyu@users.noreply.github.com>
2019-09-09 19:01:24 +0900

    Update Blink.h

--- Blink.h
+++ Blink.h
@@ -3,5 +3,30 @@
 
 class Blink
 {
+  private:
+    int pin;
+    int delay;
   public:
-    
+    void on(int pin);
+    void off(int pin);
+};
+
+
+Blink::Blink(int pin)
+{
+  pinMode(pin, OUTPUT); 
+}
+void Blink::on(int delay)
+{
+  delay(delay);
+  digitalWrite(pin, HIGH);
+  Serial.println("high");  
+}
+void Blink::off(int delay)
+{
+  delay(delay);
+  digitalWrite(pin, LOW);
+  Serial.println("low");
+}
+
+
