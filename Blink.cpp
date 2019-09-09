f1d87fe524582669f638edd71670780079db227e
Jeon-YoungGyu <55074084+Jeon-YoungGyu@users.noreply.github.com>
2019-09-09 19:21:12 +0900

    Update Blink.cpp

--- Blink.cpp
+++ Blink.cpp
@@ -1,1 +1,20 @@
+#include "Blink.h"
+
+Blink::Blink(int pin)
+{
+  pinMode(pin, OUTPUT); 
+}
+void Blink::on(int delay)
+{
+  delay(delay);
+  digitalWrite(pin, HIGH);
+  Serial.println("high");  
+}
+void Blink::off(int delay)
+{
+  delay(delay);
+  digitalWrite(pin, LOW);
+  Serial.println("low");
+}
+
 
